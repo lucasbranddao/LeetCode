cb42fe03e09a9a033f2c3266e07cae6faa4fe25e
Lucas Brandão <lucasbranddao@hotmail.com>
2024-07-03 02:22:50 -0300

    adding 2 solved problems

---
+++ ContainsDuplicate.swift
@@ -0,0 +1,14 @@
+class Solution {
+    func containsDuplicate(_ nums: [Int]) -> Bool {
+        var set: Set<Int> = []
+        for num in nums {
+            if set.contains(num) {
+                return true
+            }
+            else {
+                set.insert(num)
+            }
+        }
+        return false
+    }
+}
