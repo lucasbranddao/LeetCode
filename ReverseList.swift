d72173b76518cfcac607f9998c77d56d0a754b2e
Lucas Brandão <lucasbranddao@hotmail.com>
2024-07-11 23:59:09 -0300

    test added

---
+++ ReverseList.swift
@@ -0,0 +1,22 @@
+public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    public init() { self.val = 0; self.next = nil; }
+    public init(_ val: Int) { self.val = val; self.next = nil; }
+    public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+}
+
+class Solution {
+    func reverseList(_ head: ListNode?) -> ListNode? {
+        reverse(nil, head)
+    }
+
+    func reverse(_ prev: ListNode?, _ current: ListNode?) -> ListNode? {
+        guard let current = current else {
+            return prev
+        }
+        let next = current.next
+        current.next = prev
+        return reverse(current, next)
+    }
+}
